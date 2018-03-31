3b69de29c4dddb17565c84318c0a246159b862e9
zahavia <amirz@hpe.com>
2018-03-31 18:43:02 +0300

    House robber - remove redundant code

--- src/DynamicProgramming/HouseRobber.java
+++ src/DynamicProgramming/HouseRobber.java
@@ -5,28 +5,23 @@ class HouseRobber {
 
 	public static void main(String[] args) {
 		System.out.println(rob(new int[]{2, 1, 1, 2}));
+		System.out.println(rob(new int[]{2}));
 	}
 
 	private static int rob(int[] nums) {
 		if (nums.length == 0) {
 			return 0;
 		}
-		if (nums.length == 1) {
-			return nums[0];
-		}
-		if (nums.length == 2) {
-			return Math.max(nums[0], nums[1]);
-		}
-		int maxExludingPrev = nums[0];
-		int maxIncludingPrev = Math.max(nums[0], nums[1]);
+		int maxExcludingPrev = 0;
+		int maxIncludingPrev = 0;
 
-		for (int i = 2; i < nums.length; i++) {
-			int maxWithCellI = maxExludingPrev + nums[i];
-			if (maxWithCellI < maxIncludingPrev) {
-				maxWithCellI = maxIncludingPrev;
+		for (int num : nums) {
+			int maxWithCurrentCell = maxExcludingPrev + num;
+			if (maxWithCurrentCell < maxIncludingPrev) {
+				maxWithCurrentCell = maxIncludingPrev;
 			}
-			maxExludingPrev = maxIncludingPrev;
-			maxIncludingPrev = maxWithCellI;
+			maxExcludingPrev = maxIncludingPrev;
+			maxIncludingPrev = maxWithCurrentCell;
 		}
 		return maxIncludingPrev;
 	}
