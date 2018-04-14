b854b794d6124a06d7936cdd1baa759e48e61627
zahavia <amirz@hpe.com>
2018-04-14 19:45:04 +0300

    Work Break problem - save the words that are used in the dictionary

--- src/DynamicProgramming/WorkBreak.java
+++ src/DynamicProgramming/WorkBreak.java
@@ -1,7 +1,6 @@
 package DynamicProgramming;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 //https://www.geeksforgeeks.org/dynamic-programming-set-32-word-break-problem/
 //Given an input string and a dictionary of words, find out if the input string can be segmented into a space-separated sequence of dictionary words. See following examples for more details.
@@ -23,30 +22,37 @@ public class WorkBreak {
 
 	public static void main(String[] args) {
 		WorkBreak workBreak = new WorkBreak();
-		System.out.println(workBreak.wordBreak("GoodMorning", Arrays.asList("Good", "Mor", "ning")));
+		workBreak.wordBreak("YouGoodMorning", Arrays.asList("Good", "Morning", "to", "You"));
 
 	}
 
-	public boolean wordBreak(String s, List<String> wordDict) {
+	private boolean wordBreak(String s, List<String> wordDict) {
 		int[] memo = new int[s.length()];
 		java.util.Arrays.fill(memo, -1);
-		return canWordBreak(s, 0, wordDict, memo);
-
+		Set<String> wordsFound = new HashSet<>();
+		boolean ret = canWordBreak(s, 0, wordDict, memo, wordsFound);
+		System.out.println(ret);
+		System.out.println("words found: " + wordsFound);
+		return ret;
 	}
 
-	private boolean canWordBreak(String w, int from, List<String> wordDict, int[] memo) {
+	private boolean canWordBreak(String w, int from, List<String> wordDict, int[] memo, Set<String> wordsFound) {
 		if (from >= w.length()) return true;
 
 		if (memo[from] != -1) {
-			return memo[from] == 0 ? false : true;
+			return memo[from] != 0;
 		}
 
 		for (int i = from; i < w.length(); i++) {
-			if (wordDict.contains(w.substring(from, i + 1))) {
-				if (canWordBreak(w, i + 1, wordDict, memo)) {
+			String wordCandidate = w.substring(from, i + 1);
+			if (wordDict.contains(wordCandidate)) {
+				if (canWordBreak(w, i + 1, wordDict, memo, wordsFound)) {
+					wordsFound.add(wordCandidate);
 					memo[from] = 1;
 					return true;
 				} else {
+					// rest of the string does not contain only words from the dictionary, so remove the candidateWord as well.
+					//wordsFound.remove(wordCandidate);
 					memo[from] = 0;
 				}
 			}
